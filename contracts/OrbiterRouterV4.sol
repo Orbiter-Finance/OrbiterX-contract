6468d54d8c15c6a5011b03dd81455d5497aec899
kakui <huanhui.boy@gmail.com>
2024-04-16 12:08:02 +0800

    v4

--- contracts/OrbiterRouterV4.sol
+++ contracts/OrbiterRouterV4.sol
@@ -7,10 +7,11 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
  * @title OrbiterXRouterV3
  * @dev A contract for batch transfers of Ether and tokens to multiple addresses.
  */
-contract OrbiterXRouter {
+contract OrbiterRouterV4 {
     using SafeERC20 for IERC20;
     bool private locked;
-    event Transfer(address indexed to, uint256 amount);
+    event TransferTo(address indexed to, uint256 amount, bytes data);
+    event TransferTo(address indexed to, uint256 amount);
 
     /**
      * @dev Modifier to prevent reentrancy attacks.
@@ -31,15 +32,41 @@ contract OrbiterXRouter {
         address[] calldata tos,
         uint[] memory values
     ) external payable nonReentrant {
-        require(tos.length == values.length, "Destination and amount arrays length mismatch");
+        require(
+            tos.length == values.length,
+            "Destination and amount arrays length mismatch"
+        );
         uint total = msg.value;
         uint value;
         for (uint i = 0; i < tos.length; i++) {
             value = values[i];
             require(total >= value, "Insufficient Balance");
             total -= value;
-            payable(tos[i]).transfer(value);
-            emit Transfer(tos[i], value);
+            (bool success, ) = tos[i].call{value: value}("");
+            require(success, "Transfer failed");
+            emit TransferTo(tos[i], value);
+        }
+        require(total == 0, "There are many extra costs");
+    }
+
+    function transfers(
+        address[] calldata tos,
+        uint[] memory values,
+        bytes[] calldata extras
+    ) external payable nonReentrant {
+        require(
+            tos.length == values.length,
+            "Destination and amount arrays length mismatch"
+        );
+        uint total = msg.value;
+        uint value;
+        for (uint i = 0; i < tos.length; i++) {
+            value = values[i];
+            require(total >= value, "Insufficient Balance");
+            total -= value;
+            (bool success, ) = tos[i].call{value: value}("");
+            require(success, "Transfer failed");
+            emit TransferTo(tos[i], value, extras[i]);
         }
         require(total == 0, "There are many extra costs");
     }
@@ -56,23 +83,45 @@ contract OrbiterXRouter {
         uint[] memory values
     ) external payable nonReentrant {
         require(msg.value == 0, "Ether not accepted");
-        require(tos.length == values.length, "Destination and amount arrays length mismatch");
+        require(
+            tos.length == values.length,
+            "Destination and amount arrays length mismatch"
+        );
         for (uint i = 0; i < tos.length; i++) {
             token.safeTransferFrom(msg.sender, tos[i], values[i]);
+            emit TransferTo(tos[i], values[i]);
+        }
+    }
+
+    function transferTokens(
+        IERC20 token,
+        address[] calldata tos,
+        uint[] memory values,
+        bytes[] calldata extras
+    ) external payable nonReentrant {
+        require(msg.value == 0, "Ether not accepted");
+        require(
+            tos.length == values.length,
+            "Destination and amount arrays length mismatch"
+        );
+        for (uint i = 0; i < tos.length; i++) {
+            token.safeTransferFrom(msg.sender, tos[i], values[i]);
+            emit TransferTo(tos[i], values[i], extras[i]);
         }
     }
 
     /**
      * @dev Transfer Ether to a specified address.
      * @param to The destination address.
-     * @param data Optional data included in the transaction.
+     * @param extra Optional data included in the transaction.
      */
     function transfer(
         address to,
-        bytes calldata data
+        bytes calldata extra
     ) external payable nonReentrant {
-        payable(to).transfer(msg.value);
-        emit Transfer(to, msg.value);
+        (bool success, ) = to.call{value: msg.value}("");
+        require(success, "Transfer failed");
+        emit TransferTo(to, msg.value, extra);
     }
 
     /**
@@ -80,15 +129,16 @@ contract OrbiterXRouter {
      * @param token The token contract address.
      * @param to The destination address.
      * @param value The amount of tokens to be transferred.
-     * @param data Optional data included in the transaction.
+     * @param extra Optional data included in the transaction.
      */
     function transferToken(
         IERC20 token,
         address to,
         uint value,
-        bytes calldata data
+        bytes calldata extra
     ) external payable nonReentrant {
         require(msg.value == 0, "Ether not accepted");
         token.safeTransferFrom(msg.sender, to, value);
+        emit TransferTo(to, msg.value, extra);
     }
 }
